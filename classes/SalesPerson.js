bb983dbbfaf0949047f8c84a749a641549388e08
adilm71 <146095499+adilm71@users.noreply.github.com>
2023-10-25 13:24:32 +0100

    adding changes classes

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,4 +1,19 @@
-class SalesPerson {}
+const { Employees } = require("./Employees");
+
+class SalesPerson extends Employees {
+  #clients = [];
+  #totalSales = 0;
+  constructor(name, position, salary, clients) {
+    super(name, position, salary);
+    this.#clients = clients;
+  }
+  getSalesNumber() {
+    return this.#totalSales;
+  }
+  makeSale(amount) {
+    this.#totalSales += amount;
+  }
+}
 
 module.exports = {
   SalesPerson,
